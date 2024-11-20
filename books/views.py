fb33c9999e1094189bd0971806bf4e95762cfa29
VorisRakhimov17 <111051805+VorisRakhimov17@users.noreply.github.com>
2024-11-20 23:32:51 +0500

    1

--- books/views.py
+++ books/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from idlelib.textview import ViewWindow
 
-# Create your views here.
+from django.shortcuts import render
+from django.views import View
+
+from books.models import Book
+
+class BooksView(View):
+    def get(self, request):
+        books = Book.objects.all()
+
+        return render(request, "books/list.html", {"books": books})
+
+class BookDetailView(View):
+    def get(self, request, pk):
+        book = Book.objects.get(pk=pk)
+        return render(request, "books/detail.html", {"book": book})
+
